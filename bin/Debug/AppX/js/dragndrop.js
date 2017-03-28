5ae131139fff690847dc5d9a35798e954e3cc91d
welchts5 <welchts5@gmail.com>
2017-03-28 04:08:16 -0500

    javascript ImageMover

---
+++ bin/Debug/AppX/js/dragndrop.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $("#sortable").sortable().disableSelection();
+});
+
+$(function () {
+    var parent = $("#sortable");
+    var pics = parent.children();
+    while (pics.length) {
+        parent.append(pics.splice(Math.floor(Math.random() * pics.length), 1)[0]);
+    }
+});
+
